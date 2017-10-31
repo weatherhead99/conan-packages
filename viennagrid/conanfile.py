e336ba70a6392024db124d927778c911fb8e1a60
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2017-10-31 14:55:26 +0000

    skeleton for viennagrid

---
+++ viennagrid/conanfile.py
@@ -0,0 +1,41 @@
+from conans import ConanFile, CMake, tools
+
+class ViennagridConan(ConanFile):
+    name = "viennagrid"
+    version = "2.1.0"
+    license = "<Put the package license here>"
+    url = "github.com/weatherhead99/conan-packages.git/viennagrid"
+    description = "<Description of Viennagrid here>"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/memsharded/hello.git")
+        self.run("cd hello && git checkout static_shared")
+        # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
+        # if the packaged project doesn't have variables to set it properly
+        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)", '''PROJECT(MyHello)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_dir="%s/hello" % self.source_folder)
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s' % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+        
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
